8a7c19b78aa06e14fa96c00ee3519f86f242458d
Chalita01 <sharaqp1997@gmail.com>
2026-09-23 18:35:09 -0500

    Add files via upload

---
+++ pokedex-aprendizaje/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'pokedidiario-v1';
+const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  })));
+});
